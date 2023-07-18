c656cdb896b73baabb793013c021abfebc55748a
hoanln <hoanngocle.93@gmail.com>
2023-07-18 21:33:39 +0700

    add font google

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}'
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+            },
+            fontFamily: {
+                sans: ['var(--font-inter)'],
+                mono: ['var(--font-roboto-mono)']
+            },
+            colors: {
+                dark: '#1b1b1b',
+                light: '#f5f5f5',
+                primary: '#B63E96',
+                primaryDark: '#58E609'
+            }
+        }
     },
-  },
-  plugins: [],
-}
+    plugins: []
+};
