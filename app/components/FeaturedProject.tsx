ebc8f125e68b11e52caf6de40e5113841475be83
hoanngocle <hoanngocle.93@gmail.com>
2023-07-26 13:55:49 +0700

    Add featured project page

---
+++ app/components/FeaturedProject.tsx
@@ -0,0 +1,44 @@
+import Image, { StaticImageData } from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import { GithubIcon } from './Icons';
+
+type Props = {
+    type: string;
+    title: string;
+    summary: string;
+    img: StaticImageData;
+    link: string;
+    github: string;
+};
+
+const FeaturedProject = (props: Props) => {
+    return (
+        <article className='w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-xl'>
+            <Link href={props.link} target='_blank' className='w-1/2 cursor-pointer overflow-hidden rounded-lg '>
+                <Image src={props.img} alt={props.title} className='w-full h-auto' />
+            </Link>
+            <div className='w-1/2 flex flex-col items-start justify-between pl-6'>
+                <span className='text-primary font-medium text-xl'>{props.type}</span>
+                <Link href={props.link} target='_blank' className='hover:underline underline-offset-2'>
+                    <h2 className='my-2 w-full text-left text-4xl font-bold'>{props.title}</h2>
+                </Link>
+                <p className='my-2 font-medium text-dark'>{props.summary}</p>
+                <div className='mt-2 flex items-center'>
+                    <Link href={props.github} target='_blank' className='w-10'>
+                        <GithubIcon className='' />
+                    </Link>
+                    <Link
+                        href={props.link}
+                        target='_blank'
+                        className='ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold'
+                    >
+                        Visit Project
+                    </Link>
+                </div>
+            </div>
+        </article>
+    );
+};
+
+export default FeaturedProject;
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,6 +2,8 @@ import { Metadata } from 'next';
 import React from 'react';
 import AnimatedText from '../components/elements/AnimatedText';
 import Layout from '../components/Layout';
+import FeaturedProject from '../components/FeaturedProject';
+import ProjectCrypto from '../../public/images/projects/crypto-screener-cover-image.jpg';
 
 type Props = {};
 
@@ -14,7 +16,32 @@ const Projects = (props: Props) => {
     return (
         <article className='flex flex-col items-center w-full justify-center'>
             <Layout className=''>
-                <AnimatedText className='' text='Imagination Trumps Knowledge!'></AnimatedText>
+                <AnimatedText className='mb-16' text='Imagination Trumps Knowledge!'></AnimatedText>
+
+                <div className='grid grid-cols-12 gap-24'>
+                    <div className='col-span-12'>
+                        <FeaturedProject
+                            type={'Featured Project'}
+                            title={'React Portfolio Website'}
+                            summary='A professional portfolio website using React JS, Framer-motion, and Styled-components. It has smooth 
+page transitions, cool background effects, unique design and it is mobile responsive.
+'
+                            img={ProjectCrypto}
+                            link={'/'}
+                            github={'/'}
+                        ></FeaturedProject>
+                    </div>
+
+                    <div className='col-span-6'>Project-1</div>
+
+                    <div className='col-span-6'>Project-2</div>
+
+                    <div className='col-span-12'>Featured Project</div>
+
+                    <div className='col-span-6'>Project-3</div>
+
+                    <div className='col-span-6'>Project-4</div>
+                </div>
             </Layout>
         </article>
     );
